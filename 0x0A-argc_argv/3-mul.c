980daf39409f3bdc1042506a55aa67e16b7a0689
Cardigold <khadijatolusegun@gmail.com>
2023-06-03 01:27:06 -0700

    mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -49,6 +49,7 @@ int main(int argc, char *argv[])
 {
 int result, num1, num2;
 if (argc < 3 || argc > 3)
+{
 printf("Error\n");
 return (1);
 }
@@ -58,6 +59,3 @@ result = num1 *num2;
 printf("%d\n", result);
 return (0);
 }
-
-
-
